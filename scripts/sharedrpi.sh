8dcbee6f37840675d06f442ddb9d10d18e5e5782
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2021-02-23 22:11:35 +0100

    add smbcred and modify script shared

--- scripts/sharedrpi.sh
+++ scripts/sharedrpi.sh
@@ -3,5 +3,5 @@ if mountpoint -q "/mnt/sharedrpi";
 then
 	echo "Shared volume mount = OK"
 else
-	mount -t cifs //IP/SHAREDRPI /mnt/sharedrpi -o username=USER,password=PASS,domain=DOMAIN/HOSTNAME
+	mount -t cifs //IP/SHAREDRPI /mnt/sharedrpi -o credentials=/scripts/.smbcredentials,iocharset=utf8
 fi
